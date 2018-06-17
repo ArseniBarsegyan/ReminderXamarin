3007a462f78295eb62c3c965681af2852ba90be7
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2018-06-17 23:59:07 +0300

    Fixed editor behavior on Android (can't check on iOS)

---
+++ ReminderXamarin/ReminderXamarin/ReminderXamarin.Android/Renderers/EditorRenderer.cs
@@ -0,0 +1,28 @@
+﻿using Android.Content;
+using Android.Text;
+using Android.Views.InputMethods;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using EditorRenderer = ReminderXamarin.Droid.Renderers.EditorRenderer;
+
+[assembly: ExportRenderer(typeof(Editor), typeof(EditorRenderer))]
+namespace ReminderXamarin.Droid.Renderers
+{
+    public class EditorRenderer : Xamarin.Forms.Platform.Android.EditorRenderer
+    {
+        public EditorRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Editor> e)
+        {
+            base.OnElementChanged(e);
+            if (Control != null)
+            {
+                Control.Background = null;
+                Control.InputType = InputTypes.TextFlagCapSentences;
+                Control.SetSingleLine(false);
+            }
+        }
+    }
+}
