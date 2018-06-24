5bde722d3efd7f5ab40946d795546234837d7e1a
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2018-06-24 19:33:44 +0300

    Added AchievementModel, AchievementNote

---
+++ ReminderXamarin/ReminderXamarin/ReminderXamarin/Models/AchievementModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using SQLite;
+using SQLiteNetExtensions.Attributes;
+
+namespace ReminderXamarin.Models
+{
+    [Table("Achievements")]
+    public class AchievementModel
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string ImageUrl { get; set; }
+        public string Title { get; set; }
+        public int GeneralTimeSpent { get; set; }
+
+        [OneToMany(CascadeOperations = CascadeOperation.All)]
+        public List<AchievementNote> AchievementNotes { get; set; }
+    }
+}
---
+++ ReminderXamarin/ReminderXamarin/ReminderXamarin/Models/AchievementNote.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using SQLite;
+
+namespace ReminderXamarin.Models
+{
+    [Table("AchievementNotes")]
+    public class AchievementNote
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public int HoursSpent { get; set; }
+        public DateTime From { get; set; }
+        public DateTime To { get; set; }
+    }
+}
