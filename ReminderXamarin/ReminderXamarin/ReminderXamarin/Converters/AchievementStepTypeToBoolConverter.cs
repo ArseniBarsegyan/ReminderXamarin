cb3cad61ca146a83a86b64fb9c9cc82da24a9212
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2019-05-24 16:13:43 +0300

    Visibility of buttons for AchievementStep fix.

---
+++ ReminderXamarin/ReminderXamarin/ReminderXamarin/Converters/AchievementStepTypeToBoolConverter.cs
@@ -0,0 +1,21 @@
+﻿using ReminderXamarin.ViewModels;
+using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace ReminderXamarin.Converters
+{
+    public class AchievementStepTypeToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var achievementType = (AchievementStepType)value;
+            return achievementType == AchievementStepType.TimeSpending;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
