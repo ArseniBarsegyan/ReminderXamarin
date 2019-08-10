27618429d97856ec9f6ce2295a1d7a5ed7cb32aa
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2019-08-10 13:52:26 +0300

    Notes list remade with DataTemplateSelector

---
+++ ReminderXamarin/ReminderXamarin/ReminderXamarin/Helpers/NotesListDataTemplateSelector.cs
@@ -0,0 +1,24 @@
+﻿using ReminderXamarin.Extensions;
+using ReminderXamarin.ViewModels;
+using Xamarin.Forms;
+
+namespace ReminderXamarin.Helpers
+{
+    public class NotesListDataTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate ImageCellTemplate { get; set; }
+        public DataTemplate TextCellTemplate { get; set; }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            if (item is NoteViewModel viewModel)
+            {
+                if (!viewModel.GalleryItemsViewModels.IsNullOrEmpty())
+                {
+                    return ImageCellTemplate;
+                }
+            }
+            return TextCellTemplate;
+        }
+    }
+}
